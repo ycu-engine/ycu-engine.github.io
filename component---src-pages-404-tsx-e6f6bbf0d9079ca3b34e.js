79d2cbde30666474aa6186cc33c650dec5522b9a
YutaUra <YutaUra@users.noreply.github.com>
2021-01-20 23:40:09 +0000

    deploy: 2f3ec9856d04fe1bda786dfdda6adcff57254d2c

--- component---src-pages-404-tsx-e6f6bbf0d9079ca3b34e.js
+++
@@ -1,2 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[2],{"i6+/":function(e,t,n){"use strict";n.r(t);var u=n("q1tI");t.default=function(){return u.createElement("div",null,u.createElement("h1",null,"NOT FOUND"),u.createElement("p",null,"You just hit a route that doesn't exist... the sadness."))}}}]);
-//# sourceMappingURL=component---src-pages-404-tsx-e6f6bbf0d9079ca3b34e.js.map
---
+++ component---src-templates-member-tsx-6ae5ab7ced3c867e4a91.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[10],{"6ffk":function(e,n,t){"use strict";t.r(n);var r=t("uy1q"),a=t("Wbzz"),l=t("q1tI");n.default=function(e){var n=e.data.member;if(!n)throw Error("Member not found");return l.createElement("div",null,l.createElement("div",null,l.createElement(a.a,{to:"/members"},"メンバー一覧")),l.createElement("div",null,l.createElement(r.a,{memberName:n.name,size:"xl"})),l.createElement("div",null,n.nameJa))}}}]);
+//# sourceMappingURL=component---src-templates-member-tsx-6ae5ab7ced3c867e4a91.js.map
