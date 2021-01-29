10e21a814772603bff0c5503466c68d28690ebb6
mokroke <78187015+mokroke@users.noreply.github.com>
2021-01-29 11:35:02 +0900

    <KentaroMorota>を追加しました

--- src/data/member.ts
+++ src/data/member.ts
@@ -11,6 +11,7 @@ export const memberName = [
   'FujitaItsuki',
   'ShoKohiyama',
   'KotaroTakamori',
+  'KentaroMorota',
 ] as const
 
 export type MemberName = typeof memberName[number]
@@ -113,5 +114,11 @@ export const members: readonly Member[] = [
     nameJa: '高森 皓太郎',
     joinedAt: zonedTimeToUtc('2021-01-28', 'Asia/Tokyo'),
     belongs: { facultyName: 'DataScienceUndergraduate', grade: 3 },
+  },
+  {
+    name: 'KentaroMorota',
+    nameJa: '諸田　健太朗',
+    joinedAt: zonedTimeToUtc('2021-01-28', 'Asia/Tokyo'),
+    belongs: { facultyName: 'DataScienceUndergraduate', grade: 1 },
   },
 ]
