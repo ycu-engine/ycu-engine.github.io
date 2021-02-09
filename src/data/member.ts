e19b5f75bb49c696489b5916bffe7cd74a5a8e6d
Rereren86 <78711606+Rereren86@users.noreply.github.com>
2021-02-09 22:07:21 +0900

    メンバーを修正しました。

--- src/data/member.ts
+++ src/data/member.ts
@@ -146,13 +146,8 @@ export const members: readonly Member[] = [
   {
     name: 'RenAgo',
     nameJa: '吾郷廉',
-<<<<<<< Updated upstream
-    joinedAt: zonedTimeToUtc('2021-02-09','Asia/Tokyo'),
-    belongs: {facultyName: 'DataScienceUndergraduate',grade: 1},
-=======
     joinedAt: zonedTimeToUtc('2021-02-09', 'Asia/Tokyo'),
     belongs: { facultyName: 'DataScienceUndergraduate', grade: 1},
->>>>>>> Stashed changes
   },
 ]
 
